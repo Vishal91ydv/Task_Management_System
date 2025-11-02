3fe5db5e266f1153fadd9fa637de4183bd8f634c
vishalyadav29279 <vishalyadav29279@gmail.com>
2025-11-02 20:44:27 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ require('dotenv').config();
 const cors = require('cors');
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
 // Database Connection
 connectDB(process.env.MONGODB_URI)
 
-app.get("/",()=>{
-    console.log("Home Page")
+const auth = require('./middleware/auth');
+app.get('/api/protected', auth, (req, res) => {
+  res.json({ message: 'You accessed a protected route', user: req.user });
+});
+
+app.get("/",(req,res)=>{
+    res.send("Home page");
 })
 
 app.listen(process.env.PORT,()=>{
